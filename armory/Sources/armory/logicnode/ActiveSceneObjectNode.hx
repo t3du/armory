a4f0b4660c455baa03d6ce774ae7acc9c4fe1554
Repe <32546729+t3du@users.noreply.github.com>
2025-11-08 11:58:59 -0300

    scene object

---
+++ armory/Sources/armory/logicnode/ActiveSceneObjectNode.hx
@@ -0,0 +1,10 @@
+package armory.logicnode;
+
+class ActiveSceneObjectNode extends LogicNode {
+
+	public function new(tree: LogicTree) {
+		super(tree);
+	}
+
+	override function get(from: Int): Dynamic { return iron.Scene.active; }
+}
---
+++ armory/blender/arm/logicnode/scene/LN_get_scene_active_object.py
@@ -0,0 +1,10 @@
+from arm.logicnode.arm_nodes import *
+
+class ActiveSceneObjectNode(ArmLogicTreeNode):
+    """Returns the active scene."""
+    bl_idname = 'LNActiveSceneObjectNode'
+    bl_label = 'Get Scene Active Object'
+    arm_version = 1
+
+    def arm_init(self, context):
+        self.add_output('ArmDynamicSocket', 'Scene')
