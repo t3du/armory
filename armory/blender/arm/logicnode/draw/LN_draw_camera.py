5a19345611e7a2047855e7d6701acac9bc4cc93a
Repe <32546729+t3du@users.noreply.github.com>
2025-12-04 10:18:58 -0300

    drawCamera 1 camera

--- armory/blender/arm/logicnode/draw/LN_draw_camera.py
+++ armory/blender/arm/logicnode/draw/LN_draw_camera.py
@@ -16,50 +16,21 @@ class DrawCameraNode(ArmLogicTreeNode):
     bl_idname = 'LNDrawCameraNode'
     bl_label = 'Draw Camera'
     arm_section = 'draw'
-    arm_version = 2
-    min_inputs = 7
+    arm_version = 3
 
     num_choices: IntProperty(default=0, min=0)
-
-    def __init__(self):
-        super(DrawCameraNode, self).__init__()
-        array_nodes[str(id(self))] = self
 
     def arm_init(self, context):
         self.add_input('ArmNodeSocketAction', 'Start')
         self.add_input('ArmNodeSocketAction', 'Stop')
-        self.add_sockets()
-
-        self.add_output('ArmNodeSocketAction', 'On Start')
-        self.add_output('ArmNodeSocketAction', 'On Stop')
-
-    def add_sockets(self):
-        self.num_choices += 1
         self.add_input('ArmNodeSocketObject', 'Camera ' + str(self.num_choices))
         self.add_input('ArmIntSocket', 'X')
         self.add_input('ArmIntSocket', 'Y')
         self.add_input('ArmIntSocket', 'Width')
         self.add_input('ArmIntSocket', 'Height')
 
-    def remove_sockets(self):
-        if self.num_choices > 1:
-            for _ in range(5):
-                self.inputs.remove(self.inputs.values()[-1])
-            self.num_choices -= 1
-
-    def draw_buttons(self, context, layout):
-        row = layout.row(align=True)
-
-        op = row.operator('arm.node_call_func', text='Add Camera', icon='PLUS', emboss=True)
-        op.node_index = str(id(self))
-        op.callback_name = 'add_sockets'
-
-        column = row.column(align=True)
-        op = column.operator('arm.node_call_func', text='', icon='X', emboss=True)
-        op.node_index = str(id(self))
-        op.callback_name = 'remove_sockets'
-        if len(self.inputs) == self.min_inputs:
-            column.enabled = False
+        self.add_output('ArmNodeSocketAction', 'On Start')
+        self.add_output('ArmNodeSocketAction', 'On Stop')
 
     def get_replacement_node(self, node_tree: bpy.types.NodeTree):
         if self.arm_version not in (0, 1):
