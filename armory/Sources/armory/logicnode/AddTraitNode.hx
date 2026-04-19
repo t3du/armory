77b4b301fd60e662f211a5797034c2b0f96a9326
Repe <32546729+t3du@users.noreply.github.com>
2026-04-19 16:54:33 -0400

    update addtrait

--- armory/Sources/armory/logicnode/AddTraitNode.hx
+++ armory/Sources/armory/logicnode/AddTraitNode.hx
@@ -4,25 +4,38 @@ import iron.object.Object;
 
 class AddTraitNode extends LogicNode {
 
+	public var property0: String;
+	var trait: Dynamic;
+
 	public function new(tree: LogicTree) {
 		super(tree);
 	}
 
 	override function run(from: Int) {
 		var object: Object = inputs[1].get();
-		var traitName: String = inputs[2].get();
-
 		assert(Error, object != null, "Object should not be null");
-		assert(Error, traitName != null, "Trait name should not be null");
 
-		var cname = Type.resolveClass(Main.projectPackage + "." + traitName);
-		if (cname == null) cname = Type.resolveClass(Main.projectPackage + ".node." + traitName);
-		assert(Error, cname != null, 'No trait with the name "$traitName" found, make sure that the trait is exported!');
-		assert(Warning, object.getTrait(cname) == null, 'Object already has the trait "$traitName" applied');
+		if (property0 == 'TraitName'){
+			var traitName: String = inputs[2].get();
 
-		var trait = Type.createInstance(cname, []);
+			assert(Error, traitName != null, "Trait name should not be null");
+
+			var cname = Type.resolveClass(Main.projectPackage + "." + traitName);
+			if (cname == null) cname = Type.resolveClass(Main.projectPackage + ".node." + traitName);
+			assert(Error, cname != null, 'No trait with the name "$traitName" found, make sure that the trait is exported!');
+			assert(Warning, object.getTrait(cname) == null, 'Object already has the trait "$traitName" applied');
+
+			trait = Type.createInstance(cname, []);
+
+		} else
+			trait = inputs[2].get();		
+
 		object.addTrait(trait);
 
 		runOutput(0);
+	}
+
+	override function get(from: Int): Dynamic {
+		return trait;
 	}
 }
--- armory/blender/arm/logicnode/trait/LN_add_trait_to_object.py
+++ armory/blender/arm/logicnode/trait/LN_add_trait_to_object.py
@@ -4,11 +4,35 @@ class AddTraitNode(ArmLogicTreeNode):
     """Adds trait to the given object."""
     bl_idname = 'LNAddTraitNode'
     bl_label = 'Add Trait to Object'
-    arm_version = 2
+    arm_version = 3
+
+    def remove_extra_inputs(self, context):
+        if len(self.inputs) > 1:
+            self.inputs.remove(self.inputs[-1])
+        if self.property0 == 'Trait':
+            self.add_input('ArmDynamicSocket', 'Trait')
+        else:
+            self.add_input('ArmStringSocket', 'TraitName')
+
+    property0: HaxeEnumProperty(
+    'property0',
+    items = [('Trait', 'Trait', 'Trait'),
+             ('TraitName', 'TraitName', 'TraitName')],
+    name='', default='TraitName', update=remove_extra_inputs)
 
     def arm_init(self, context):
         self.add_input('ArmNodeSocketAction', 'In')
         self.add_input('ArmNodeSocketObject', 'Object')
-        self.add_input('ArmStringSocket', 'Trait')
+        self.add_input('ArmStringSocket', 'TraitName')
 
         self.add_output('ArmNodeSocketAction', 'Out')
+        self.add_output('ArmDynamicSocket', 'Trait')
+
+    def draw_buttons(self, context, layout):
+        layout.prop(self, 'property0')
+
+    def get_replacement_node(self, node_tree: bpy.types.NodeTree):
+        if self.arm_version not in (0, 2):
+            raise LookupError()
+            
+        return NodeReplacement.Identity(self)
