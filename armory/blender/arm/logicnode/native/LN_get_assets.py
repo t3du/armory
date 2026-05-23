ab5ca8861292996751b5498938b033cebe84dce3
Repe <32546729+t3du@users.noreply.github.com>
2026-05-23 13:42:56 -0400

    erase comment

--- armory/blender/arm/logicnode/native/LN_get_assets.py
+++ armory/blender/arm/logicnode/native/LN_get_assets.py
@@ -1,10 +1,7 @@
 from arm.logicnode.arm_nodes import *
 
 class GetAssetsNode(ArmLogicTreeNode):
-    """Searches for a collection of objects with the given name and
-    outputs the collection's objects as an array, if found.
-
-    @seeNode Collection"""
+    """"""
     bl_idname = 'LNGetAssetsNode'
     bl_label = 'Get Assets'
     arm_section = 'Assets'
