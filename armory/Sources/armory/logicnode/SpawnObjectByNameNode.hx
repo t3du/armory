257efb08fb7b5f258d7f3b82a8912d75a053d40b
Repe <32546729+t3du@users.noreply.github.com>
2026-06-08 11:28:19 -0400

    update spawn nodes

--- armory/Sources/armory/logicnode/SpawnObjectByNameNode.hx
+++ armory/Sources/armory/logicnode/SpawnObjectByNameNode.hx
@@ -9,7 +9,6 @@ import armory.trait.physics.RigidBody;
 class SpawnObjectByNameNode extends LogicNode {
 
 	var object: Object;
-	var matrices: Array<Mat4> = [];
 
 	/** Scene from which to take the object **/
 	public var property0: Null<String>;
@@ -22,17 +21,18 @@ class SpawnObjectByNameNode extends LogicNode {
 		var objectName = inputs[1].get();
 		if (objectName == null) return;
 
+		var sceneName = property0;
 		#if arm_json
-		property0 += ".json";
+		sceneName += ".json";
 		#elseif arm_compress
-		property0 += ".lz4";
+		sceneName += ".lz4";
 		#end
 
 		var m: Mat4 = inputs[2].get();
-		matrices.push(m != null ? m.clone() : null);
-		var spawnChildren: Bool = inputs.length > 3 ? inputs[3].get() : true; // TODO
+		var matrix: Mat4 = m != null ? m.clone() : null;
+		var spawnChildren: Bool = inputs[3].get();
 
-		Data.getSceneRaw(property0, (rawScene: TSceneFormat) -> {
+		Data.getSceneRaw(sceneName, (rawScene: TSceneFormat) -> {
 
 			//Check if object with given name present in the specified scene
 			var objPresent: Bool = false;
@@ -48,7 +48,6 @@ class SpawnObjectByNameNode extends LogicNode {
 			//Spawn object if present
 			iron.Scene.active.spawnObject(objectName, null, function(o: Object) {
 				object = o;
-				var matrix = matrices.pop(); // Async spawn in a loop, order is non-stable
 				if (matrix != null) {
 					object.transform.setMatrix(matrix);
 					#if arm_physics
--- armory/Sources/armory/logicnode/SpawnObjectNode.hx
+++ armory/Sources/armory/logicnode/SpawnObjectNode.hx
@@ -7,7 +7,6 @@ import armory.trait.physics.RigidBody;
 class SpawnObjectNode extends LogicNode {
 
 	var object: Object;
-	var matrices: Array<Mat4> = [];
 
 	public function new(tree: LogicTree) {
 		super(tree);
@@ -21,12 +20,11 @@ class SpawnObjectNode extends LogicNode {
 		if (objectName == "") objectName = tree.object.name;
 
 		var m: Mat4 = inputs[2].get();
-		matrices.push(m != null ? m.clone() : null);
-		var spawnChildren: Bool = inputs.length > 3 ? inputs[3].get() : true; // TODO
+		var matrix: Mat4 = m != null ? m.clone() : null;
+		var spawnChildren: Bool = inputs[3].get();
 
 		iron.Scene.active.spawnObject(objectName, null, function(o: Object) {
 			object = o;
-			var matrix = matrices.pop(); // Async spawn in a loop, order is non-stable
 			if (matrix != null) {
 				object.transform.setMatrix(matrix);
 				#if arm_physics
