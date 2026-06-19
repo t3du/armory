61de5e1420cb01d1ae8ff8ad5270f872294c6395
Repe <32546729+t3du@users.noreply.github.com>
2026-06-18 21:23:08 -0400

    Update GetLightDataNode.hx

--- armory/Sources/armory/logicnode/GetLightDataNode.hx
+++ armory/Sources/armory/logicnode/GetLightDataNode.hx
@@ -16,10 +16,17 @@ class GetLightDataNode extends LogicNode {
 		return switch (from) {
 			case 0:
 				return iron.data.LightData.typeToInt(light.data.raw.type);
+			#if arm_SinglePoint	
 			case 1:
 				return light.data.raw.type == "sun" ? light.data.raw.strength / 0.325 : light.data.raw.strength / 0.01;
 			case 2:
 				return new iron.math.Vec4(light.data.raw.color[0],  light.data.raw.color[1], light.data.raw.color[2]);
+			#else
+			case 1:
+				return light.data.raw.type == "sun" ? light.strength / 0.325 : light.strength / 0.01;
+			case 2:
+				return light.color;
+			#end
 			case 3:
 				light.data.raw.cast_shadow;
 			default:
