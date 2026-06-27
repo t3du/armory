92af6a845624f754178acb7d9cd32ac4e1883864
Repe <32546729+t3du@users.noreply.github.com>
2026-06-27 00:00:52 -0400

    get curve points node

---
+++ armory/blender/arm/logicnode/curve/LN_get_curve_points.py
@@ -0,0 +1,16 @@
+from arm.logicnode.arm_nodes import *
+
+class GetCurvePointsNode(ArmLogicTreeNode):
+    """Gets curve spline control points."""
+    bl_idname = 'LNGetCurvePointsNode'
+    bl_label = 'Get Curve Points'
+    arm_section = 'Curve'
+    arm_version = 1
+
+    def arm_init(self, context):
+        self.add_input('ArmNodeSocketObject', 'Curve')
+        self.add_input('ArmIntSocket', 'Spline Index')
+
+        self.add_output('ArmNodeSocketArray', 'Points')
+        self.add_output('ArmBoolSocket', 'Closed')
+
