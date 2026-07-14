235ffb4143104cce7c91f370c1f58f284006acea
Repe <32546729+t3du@users.noreply.github.com>
2026-07-14 16:50:27 -0400

    Update RetainValueNode.hx

--- armory/Sources/armory/logicnode/RetainValueNode.hx
+++ armory/Sources/armory/logicnode/RetainValueNode.hx
@@ -15,16 +15,18 @@ class RetainValueNode extends LogicNode {
 	override function run(from: Int) {
 		var retainValue = inputs[1].get();
 
-		switch(Type.getClassName(Type.getClass(retainValue))){
-			case "iron.math.Vec4":
-				value = (cast retainValue: Vec4).clone();
-			case "iron.math.Mat4":
-				value = (cast retainValue: Mat4).clone();
-			case "iron.math.Quat":
-				var q: Quat = new Quat();
-				value = q.setFrom((cast retainValue: Quat));
-			default:
-				value = retainValue;
+		if (retainValue != null){
+			switch(Type.getClassName(Type.getClass(retainValue))){
+				case "iron.math.Vec4":
+					value = (cast retainValue: Vec4).clone();
+				case "iron.math.Mat4":
+					value = (cast retainValue: Mat4).clone();
+				case "iron.math.Quat":
+					var q: Quat = new Quat();
+					value = q.setFrom((cast retainValue: Quat));
+				default:
+					value = retainValue;
+			}
 		}
 
 		runOutput(0);
