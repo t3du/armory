77db8b5368f1e46e3f576573ed4c2553a2c9103e
Repe <32546729+t3du@users.noreply.github.com>
2026-07-15 22:08:39 -0400

    pick navmesh location: include children

--- armory/Sources/armory/logicnode/PickLocationNode.hx
+++ armory/Sources/armory/logicnode/PickLocationNode.hx
@@ -1,11 +1,14 @@
 package armory.logicnode;
 
 #if arm_navigation
+import armory.trait.NavMesh;
 import armory.trait.navigation.Navigation;
 #end
 
 import iron.object.Object;
 import iron.math.Vec4;
+import iron.math.RayCaster;
+import iron.Scene;
 
 class PickLocationNode extends LogicNode {
 
@@ -20,12 +23,38 @@ class PickLocationNode extends LogicNode {
 
 		var loc = new Vec4();
 
+		#if arm_navigation
+		var navTrait: NavMesh = Navigation.active.navMeshes[0];
+		var useChildren = navTrait != null && navTrait.combineImmidiateChildren;
+		#end
+
 		#if arm_physics
 		var physics = armory.trait.physics.PhysicsWorld.active;
 		var b = physics.pickClosest(x, y);
-		var rb = object.getTrait(armory.trait.physics.RigidBody);
 
-		if (rb != null && b == rb) {
+		var hit = false;
+
+		var rb = object.getTrait(armory.trait.physics.RigidBody);
+		if (rb != null && b == rb)
+			hit = true;
+
+		if (!hit) {
+			#if arm_navigation
+			if (useChildren) {
+				for (child in object.children) {
+					if (child.raw.type != "mesh_object") continue;
+
+					var childRb = child.getTrait(armory.trait.physics.RigidBody);
+					if (childRb != null && childRb == b) {
+						hit = true;
+						break;
+					}
+				}
+			}
+			#end
+		}
+
+		if (hit) {
 			var p = physics.hitPointWorld;
 			loc.set(p.x, p.y, p.z);
 			return loc;
@@ -36,10 +65,22 @@ class PickLocationNode extends LogicNode {
 
 		#if arm_navigation
 		assert(Error, Navigation.active.navMeshes.length > 0, "No Navigation Mesh Present");
-		loc = iron.math.RayCaster.boxIntersectObject(object, x, y, iron.Scene.active.camera);
+
+		loc = RayCaster.boxIntersectObject(object, x, y, Scene.active.camera);
+
+		if (loc == null && useChildren) {
+			for (child in object.children) {
+				if (child.raw.type != "mesh_object") continue;
+
+				loc = RayCaster.boxIntersectObject(child, x, y, Scene.active.camera);
+
+				if (loc != null) break;
+			}
+		}
+
 		return loc != null ? Navigation.active.navMeshes[0].getClosestPoint(loc) : null;
 		#end
-		
+
 		return null;
 	}
 }
