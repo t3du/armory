867d14e095f0e5cbf9bc0f950d38b7afeaf46472
Repe <32546729+t3du@users.noreply.github.com>
2026-07-17 19:23:14 -0400

    randomColorNode: upgrade

--- armory/Sources/armory/logicnode/RandomColorNode.hx
+++ armory/Sources/armory/logicnode/RandomColorNode.hx
@@ -4,17 +4,27 @@ import iron.math.Vec4;
 
 class RandomColorNode extends LogicNode {
 
+	public var property0: Bool;
+	public var property1: Bool;
+	public var property2: Bool;
+	public var property3: Bool;
+
+	public var property4: Float;
+	public var property5: Float;
+	public var property6: Float;
+	public var property7: Float;
+
 	public function new(tree: LogicTree) {
 		super(tree);
 	}
 
 	override function get(from: Int): Dynamic {
-		
-		var r = Math.random();
-		var g = Math.random();
-		var b = Math.random();
-		// var a = Math.random();
-		var v = new Vec4(r, g, b);
-		return v;
+
+		var r = property0 ? Math.random() : property4;
+		var g = property1 ? Math.random() : property5;
+		var b = property2 ? Math.random() : property6;
+		var a = property3 ? Math.random() : property7;
+
+		return new Vec4(r, g, b, a);
 	}
 }
--- armory/blender/arm/logicnode/random/LN_random_color.py
+++ armory/blender/arm/logicnode/random/LN_random_color.py
@@ -1,11 +1,91 @@
 from arm.logicnode.arm_nodes import *
 
-
 class RandomColorNode(ArmLogicTreeNode):
     """Generates a random color."""
     bl_idname = 'LNRandomColorNode'
     bl_label = 'Random Color'
-    arm_version = 1
+    arm_version = 2
+
+    property0: HaxeBoolProperty(
+        'property0',
+        name='R',
+        default=True
+    )
+
+    property1: HaxeBoolProperty(
+        'property1',
+        name='G',
+        default=True
+    )
+
+    property2: HaxeBoolProperty(
+        'property2',
+        name='B',
+        default=True
+    )
+
+    property3: HaxeBoolProperty(
+        'property3',
+        name='A',
+        default=False
+    )
+
+    property4: HaxeFloatProperty(
+        'property4',
+        name='R Value',
+        default=0.0,
+        min=0.0,
+        max=1.0
+    )
+
+    property5: HaxeFloatProperty(
+        'property5',
+        name='G Value',
+        default=0.0,
+        min=0.0,
+        max=1.0
+    )
+
+    property6: HaxeFloatProperty(
+        'property6',
+        name='B Value',
+        default=0.0,
+        min=0.0,
+        max=1.0
+    )
+
+    property7: HaxeFloatProperty(
+        'property7',
+        name='A Value',
+        default=1.0,
+        min=0.0,
+        max=1.0
+    )
+
 
     def arm_init(self, context):
         self.add_output('ArmColorSocket', 'Color')
+
+
+    def draw_buttons(self, context, layout):
+
+        for flag, value, label in [
+            ('property0', 'property4', 'R'),
+            ('property1', 'property5', 'G'),
+            ('property2', 'property6', 'B'),
+            ('property3', 'property7', 'A'),
+        ]:
+            row = layout.row(align=True)
+
+            row.prop(self, flag, text=label)
+
+            value_row = row.row(align=True)
+            value_row.enabled = not getattr(self, flag)
+            value_row.prop(self, value, text='')
+
+
+    def get_replacement_node(self, node_tree):
+        if self.arm_version not in (0, 1):
+            raise LookupError()
+
+        return NodeReplacement.Identity(self)
