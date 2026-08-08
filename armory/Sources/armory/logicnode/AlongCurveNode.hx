f7a2b05b10f0398b219d44cb2633c7c56fd86414
Repe <32546729+t3du@users.noreply.github.com>
2026-08-08 11:01:07 -0400

    Update AlongCurveNode.hx

--- armory/Sources/armory/logicnode/AlongCurveNode.hx
+++ armory/Sources/armory/logicnode/AlongCurveNode.hx
@@ -29,57 +29,7 @@ class AlongCurveNode extends LogicNode {
 		var forwardAxis: String = inputs[4].get();
 		var position: Float = inputs[5].get();
 
-		var pos = curve.getPoint(position, splineIdx);
-		pos.applymat4(curve.transform.world);
-		object.transform.loc.setFrom(pos);
-
-		f.setFrom(curve.getTangent(position, splineIdx));
-		f.applyQuat(curve.transform.rot);
-		f.normalize();
-
-		z.set(0, 0, 1);
-		if (Math.abs(f.dot(z)) > 0.9999) {
-			z.set(0, 1, 0);
-		}
-
-		r.crossvecs(f, z).normalize();
-		u.crossvecs(r, f).normalize();
-
-		switch (forwardAxis) {
-			case "X":
-				vx.setFrom(f);
-				vy.set(-r.x, -r.y, -r.z);
-				vz.setFrom(u);
-			case "-X":
-				vx.set(-f.x, -f.y, -f.z);
-				vy.setFrom(r);
-				vz.setFrom(u);
-			case "Y":
-				vx.setFrom(r);
-				vy.setFrom(f);
-				vz.setFrom(u);
-			case "-Y":
-				vx.set(-r.x, -r.y, -r.z);
-				vy.set(-f.x, -f.y, -f.z);
-				vz.setFrom(u);
-			case "Z":
-				vx.setFrom(r);
-				vy.set(-u.x, -u.y, -u.z);
-				vz.setFrom(f);
-			case "-Z":
-				vx.setFrom(r);
-				vy.setFrom(u);
-				vz.set(-f.x, -f.y, -f.z);
-		}
-
-		m._00 = vx.x; m._01 = vx.y; m._02 = vx.z;
-		m._10 = vy.x; m._11 = vy.y; m._12 = vy.z;
-		m._20 = vz.x; m._21 = vz.y; m._22 = vz.z;
-
-		q.fromRotationMat(m);
-		object.transform.rot.setFrom(q);
-
-		object.transform.buildMatrix();
+		curve.follow(object, position, splineIdx, forwardAxis);
 
 		runOutput(0);
 	}
