cfb0a4a2f3c0579bb74715ee668e7e2e4b386786
Repe <32546729+t3du@users.noreply.github.com>
2026-08-23 11:49:59 -0400

    Update LN_get_rb_point_velocity.py

--- armory/blender/arm/logicnode/physics/LN_get_rb_point_velocity.py
+++ armory/blender/arm/logicnode/physics/LN_get_rb_point_velocity.py
@@ -3,11 +3,11 @@ from arm.logicnode.arm_nodes import *
 class GetPointVelocityNode(ArmLogicTreeNode):
     """Returns the world velocity of the given point along the rigid body."""
     bl_idname = 'LNGetPointVelocityNode'
-    bl_label = 'Get RB Point Velocity'
+    bl_label = 'Get RB Velocity at Location'
     arm_version = 1
 
     def arm_init(self, context):
         self.add_input('ArmNodeSocketObject', 'RB')
-        self.add_input('ArmVectorSocket', 'Point')
+        self.add_input('ArmVectorSocket', 'Location')
 
         self.add_output('ArmVectorSocket', 'Velocity')
