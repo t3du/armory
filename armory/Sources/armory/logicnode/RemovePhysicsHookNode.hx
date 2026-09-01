564a9fc5cbc3a2525dea323d945418d3fed02fd5
Repe <32546729+t3du@users.noreply.github.com>
2026-09-01 11:29:03 -0400

    remove hook hack for SB

--- armory/Sources/armory/logicnode/RemovePhysicsHookNode.hx
+++ armory/Sources/armory/logicnode/RemovePhysicsHookNode.hx
@@ -3,6 +3,9 @@ package armory.logicnode;
 import iron.object.Object;
 #if arm_bullet
 import armory.trait.physics.bullet.PhysicsHook;
+#if arm_physics_soft
+import armory.trait.physics.SoftBody;
+#end
 #end
 
 class RemovePhysicsHookNode extends LogicNode {
@@ -14,7 +17,7 @@ class RemovePhysicsHookNode extends LogicNode {
 	override function run(from: Int) {
 		var obj: Object = inputs[1].get();
 
-		if (obj == null) return;
+		if (obj == null){ runOutput(0); return; }
 
 #if arm_bullet
 		var hook = obj.getTrait(PhysicsHook);
@@ -22,6 +25,26 @@ class RemovePhysicsHookNode extends LogicNode {
 			hook.removePhysicsHook();
 			obj.removeTrait(hook);
 		}
+
+//hack: SB loses collision after removing hook
+#if arm_physics_soft
+		var softBody = obj.getTrait(SoftBody);
+		if (softBody != null) {
+			var newSoftBody = new SoftBody(
+				softBody.shape,
+				softBody.bend,
+				softBody.mass,
+				softBody.margin,
+				softBody.friction,
+				softBody.damping,
+				softBody.linearStiffness,
+				softBody.angularStiffness,
+				softBody.pressure
+			);
+			obj.removeTrait(softBody);
+			obj.addTrait(newSoftBody);
+		}
+#end
 #end
 		runOutput(0);
 	}
