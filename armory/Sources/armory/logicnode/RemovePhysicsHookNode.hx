b18ebb9167e960d6fbcad4b42b4c5b976fed494b
Repe <32546729+t3du@users.noreply.github.com>
2026-09-01 12:08:02 -0400

    remove hook hack for SB with deformations

--- armory/Sources/armory/logicnode/RemovePhysicsHookNode.hx
+++ armory/Sources/armory/logicnode/RemovePhysicsHookNode.hx
@@ -29,7 +29,22 @@ class RemovePhysicsHookNode extends LogicNode {
 //hack: SB loses collision after removing hook
 #if arm_physics_soft
 		var softBody = obj.getTrait(SoftBody);
-		if (softBody != null) {
+		if (softBody != null && softBody.body != null) {
+			@:privateAccess
+			var oldNodes = softBody.body.get_m_nodes();
+			var count = oldNodes.size();
+
+			var savedData = [];
+			for (i in 0...count) {
+				var pos = oldNodes.at(i).get_m_x();
+				var nor = oldNodes.at(i).get_m_n();
+				savedData.push({
+					x: pos.x(), y: pos.y(), z: pos.z(),
+					nx: nor.x(), ny: nor.y(), nz: nor.z()
+				});
+			}
+
+			@:privateAccess
 			var newSoftBody = new SoftBody(
 				softBody.shape,
 				softBody.bend,
@@ -41,8 +56,24 @@ class RemovePhysicsHookNode extends LogicNode {
 				softBody.angularStiffness,
 				softBody.pressure
 			);
+
 			obj.removeTrait(softBody);
 			obj.addTrait(newSoftBody);
+
+			@:privateAccess {
+				newSoftBody.init();
+				newSoftBody._init = null;
+
+				if (newSoftBody.body != null) {
+					var newNodes = newSoftBody.body.get_m_nodes();
+					for (i in 0...count) {
+						var d = savedData[i];
+						newNodes.at(i).get_m_x().setValue(d.x, d.y, d.z);
+						newNodes.at(i).get_m_n().setValue(d.nx, d.ny, d.nz);
+					}
+					newSoftBody.update();
+				}
+			}
 		}
 #end
 #end
